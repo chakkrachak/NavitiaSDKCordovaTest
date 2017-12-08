7e5da60359771c5efc5125e9b99f7b3bab499d29
Chakkra CHAK <chakkra.chak@canaltp.fr>
2017-12-08 17:09:25 +0100

    Test without dependancy

--- launchTest.sh
+++ launchTest.sh
@@ -9,11 +9,12 @@ rm -rf ./$CordovaNavitiaSDK_GitHubRepository
 git checkout . && git clean -fd
 
 git clone git@github.com:CanalTP/$CordovaNavitiaSDK_GitHubRepository.git
+
 cd CordovaAppTest
 
 ionic cordova platform add ios
 ionic cordova platform add android
-ionic cordova plugin add ../$CordovaNavitiaSDK_GitHubRepository
+#ionic cordova plugin add ../$CordovaNavitiaSDK_GitHubRepository
 
 npm rebuild node-sass --force
 
