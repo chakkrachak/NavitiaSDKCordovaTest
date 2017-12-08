151246d1b3a21f52ad0531080b147aa0ba48e9c3
Chakkra CHAK <chakkra.chak@canaltp.fr>
2017-12-08 15:34:47 +0100

    Test iOS

--- launchTest.sh
+++ launchTest.sh
@@ -10,10 +10,11 @@ git checkout . && git clean -fd
 
 git clone git@github.com:CanalTP/$CordovaNavitiaSDK_GitHubRepository.git
 cd CordovaAppTest
-#ionic cordova platform add ios
-ionic cordova platform add android
+
+ionic cordova platform add ios
+#ionic cordova platform add android
 ionic cordova plugin add ../$CordovaNavitiaSDK_GitHubRepository
 
-#ionic cordova build ios
-ionic cordova build android
+ionic cordova build ios
+#ionic cordova build android
 cd ..
