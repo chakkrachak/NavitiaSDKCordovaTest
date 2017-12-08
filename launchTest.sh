f2cd94248bb2f8685b3abfc14d75fb0eebba29fa
Chakkra CHAK <chakkra.chak@canaltp.fr>
2017-12-08 15:04:59 +0100

    Testing for android

--- launchTest.sh
+++ launchTest.sh
@@ -3,3 +3,14 @@
 # TRACING ON
 # set -x
 
+SET CordovaNavitiaSDK_GitHubRepository='CDVNavitiaSDK'
+
+git checkout . && git clean -fd
+
+git clone git@github.com:CanalTP/$CordovaNavitiaSDK_GitHubRepository.git
+#ionic cordova platform add ios
+ionic cordova platform add android
+ionic cordova plugin add ./$CordovaNavitiaSDK_GitHubRepository
+
+#ionic cordova build ios
+ionic cordova build android
