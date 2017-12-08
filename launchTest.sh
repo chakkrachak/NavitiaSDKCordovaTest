3bc639964b892bb2d8f245ef681dd313f63c4cbb
Chakkra CHAK <chakkra.chak@canaltp.fr>
2017-12-08 15:07:03 +0100

    Fixed script

--- launchTest.sh
+++ launchTest.sh
@@ -3,14 +3,15 @@
 # TRACING ON
 # set -x
 
-SET CordovaNavitiaSDK_GitHubRepository='CDVNavitiaSDK'
+CordovaNavitiaSDK_GitHubRepository='CDVNavitiaSDK'
 
 git checkout . && git clean -fd
 
 git clone git@github.com:CanalTP/$CordovaNavitiaSDK_GitHubRepository.git
+cd $CordovaNavitiaSDK_GitHubRepository
 #ionic cordova platform add ios
 ionic cordova platform add android
-ionic cordova plugin add ./$CordovaNavitiaSDK_GitHubRepository
+ionic cordova plugin add ../$CordovaNavitiaSDK_GitHubRepository
 
 #ionic cordova build ios
 ionic cordova build android
