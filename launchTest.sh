fc4452def23a7e2ef060bfd5784214c36f0f753a
Chakkra CHAK <chakkra.chak@canaltp.fr>
2017-12-11 11:25:53 +0100

    Update script

--- launchTest.sh
+++ launchTest.sh
@@ -10,7 +10,7 @@ function ensureFileExists {
     else
         echo "$* does not exist => ## PROBLEM FILE NOT FOUND ##";
         exit 1;
-    fi    
+    fi
 }
 
 function ensureFolderExists {
@@ -19,29 +19,41 @@ function ensureFolderExists {
     else
         echo "$* does not exist => ## PROBLEM FOLDER NOT FOUND ##";
         exit 1;
-    fi    
+    fi
+}
+
+# RETRIEVE NAVITIA SDK
+function retrieveNavitiaSDK_master {
+    CordovaNavitiaSDK_ProjectName='CDVNavitiaSDK'
+
+    rm -rf ./$CordovaNavitiaSDK_ProjectName
+    git checkout . && git clean -fd
+    rm -rf ./CordovaAppTest/platforms/ios/build/emulator/CordovaAppTest.app.dSYM
+    rm ./CordovaAppTest/platforms/android/build/outputs/apk/android-debug.apk
+
+    git clone git@github.com:CanalTP/$CordovaNavitiaSDK_ProjectName.git
 }
 
 # Script
+## RETRIEVE SDK
+retrieveNavitiaSDK_master
 
-CordovaNavitiaSDK_GitHubRepository='CDVNavitiaSDK'
-
-rm -rf ./$CordovaNavitiaSDK_GitHubRepository
-git checkout . && git clean -fd
-rm -rf ./CordovaAppTest/platforms/ios/build/emulator/CordovaAppTest.app.dSYM
-rm ./CordovaAppTest/platforms/android/build/outputs/apk/android-debug.apk
-
-git clone git@github.com:CanalTP/$CordovaNavitiaSDK_GitHubRepository.git
+## GO TO APP TEST FOLDER
 cd CordovaAppTest
-ionic cordova plugin add ../$CordovaNavitiaSDK_GitHubRepository
 
+## INSTALL PLUGIN
+ionic cordova plugin add ../$CordovaNavitiaSDK_ProjectName
+
+## BUILD ANDROID
 ionic cordova platform add android
 ionic cordova build android
 ensureFileExists "./platforms/android/build/outputs/apk/android-debug.apk"
 
+## BUILD IOS
 npm rebuild node-sass --force
 ionic cordova platform add ios
 ionic cordova build ios
 ensureFolderExists "./platforms/ios/build/emulator/CordovaAppTest.app.dSYM"
 
+## GO BACK TO MAIN FOLDER
 cd ..
